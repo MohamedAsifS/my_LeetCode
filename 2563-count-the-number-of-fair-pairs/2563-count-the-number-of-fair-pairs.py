88ea6899f6d41ea47bd178265d0895970ae50185
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2024-11-14 02:48:12 +0530

    Time: 915 ms (29.21%), Space: 30.6 MB (71.36%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -1,5 +1,13 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        def Bianry_search(l,r,target):
+            while l<=r:
+                m=(l+r)//2
+                if nums[m]>=target:
+                    r=m-1
+                else:
+                    l=m+1
+            return r
 
         #O(logn)
 
@@ -7,19 +15,15 @@ class Solution:
 
         nums=sorted(nums)
 
-        print(nums)
-        i=0
-        j=i+1
-
         count=0
 
         for i in range(len(nums)):
-           for j in range(i,len(nums)):
-              if nums[i]+nums[j]>upper:
-                   break
-              if nums[i] + nums[j] <=upper and nums[i]+nums[j]>=lower:
-                     count+=1
-              
+            low=lower-nums[i]
+            up=upper-nums[i]
+
+            count+=(Bianry_search(i+1,len(nums)-1,up+1)-
+            Bianry_search(i+1,len(nums)-1,low))
+
                      
              
               
