6d9d83662ea693a19c5826e5c960b302fac976c2
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2024-11-15 01:58:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        def helper(x):
+            store=0
+            for i in quantities:
+                store+=math.ceil(i/x)
+            return  store <= n
+        
+
+        l,r=0,max(quantities)
+        res=0
+
+        while l <= r:
+            m=(l+r)//2
+            if helper(m):
+                res=m
+                r=m-1
+            else:
+                l=m+1
+        return res
+
+
+
+        
+        
