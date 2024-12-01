d23ba6e7c2c5f40e217e4a2443ab6450f12a2102
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2024-12-01 08:32:09 +0530

    Time: 2 ms (93.52%), Space: 43.1 MB (33.23%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+
+        HashSet <Integer> box=new HashSet<>();
+
+        for(int i:arr){
+            if((box.contains(i/2) && i%2==0) || box.contains(i*2)){
+              
+               return true;
+        }
+        else{
+           
+            box.add(i);
+        }
+       
+    }
+     return false;
+}}
