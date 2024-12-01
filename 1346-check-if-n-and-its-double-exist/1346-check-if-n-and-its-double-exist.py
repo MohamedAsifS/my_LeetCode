f1d816974bcdf4a958e60369caee57e0e0fd3237
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2024-12-01 08:00:14 +0530

    Time: 0 ms (100%), Space: 17.4 MB (5.25%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -7,8 +7,6 @@ class Solution:
             temp=arr[i]*2
             div=arr[i]/2
             if temp in hashp or div in hashp:
-                print(hashp)
-                print(arr[i])
                 return True
             else:
                 hashp[arr[i]]=temp
