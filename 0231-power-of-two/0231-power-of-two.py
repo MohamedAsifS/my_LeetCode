9bd7dd74b18c916631f8d1f2448045f1bddf8f7b
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2024-12-04 23:24:36 +0530

    Time: 2 ms (19.25%), Space: 17.3 MB (5.4%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        
+        if bin(n).count('1')==1 and n>=0:
+          
+            return True
+        else:
+            
+            return False
+        
