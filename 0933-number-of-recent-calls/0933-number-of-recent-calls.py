1e23fdb8794579e52832e380453e9fcd90f5a1a6
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2024-12-26 19:31:59 +0530

    Time: 42 ms (52.94%), Space: 23.1 MB (13.98%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,20 +1,16 @@
 class RecentCounter:
 
     def __init__(self):
-        self.array=[]
-        self.array.append(None)
+        self.array=Deque()
+     
         
 
     def ping(self, t: int) -> int:
-        start=t-3000
-        end=t
-        res=0
         self.array.append(t)
-        for i in range(1,len(self.array)):
-                if self.array[i]>=start and self.array[i]<=end:
-                    res+=1 
+        while self.array and self.array[0]<t-3000:
+            self.array.popleft()
         
-        return res
+        return len(self.array)
 
         
 
