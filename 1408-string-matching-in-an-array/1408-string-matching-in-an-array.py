884295418e41476296f4be21694615c73cc1e635
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-01-07 17:02:24 +0530

    Time: 0 ms (100%), Space: 17.6 MB (28.23%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -2,14 +2,14 @@ class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         res=[]
         words=sorted(words,key=lambda x:len(x))
-        print(words)
+  
         for i in range(len(words)):
             for j in range(i+1,len(words)):
                 if words[i] in words[j]:
                   
                     res.append(words[i])
                     break
-        print("od" in "leetcode")
+  
         return res
 
 
