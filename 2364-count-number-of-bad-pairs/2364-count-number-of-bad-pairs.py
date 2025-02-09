0253593bd7c9d2a0c805f3d1edc6fc1a22f8e89c
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-02-10 00:25:23 +0530

    Time: 91 ms (42.38%), Space: 39 MB (28.81%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -9,9 +9,8 @@ class Solution:
 
         for i in range(len(nums)):
                total_pairs+=i
-               num=nums[i]-i
-               good_pairs+=count[num]
-               count[num]+=1
+               good_pairs+=count[nums[i]-i]
+               count[nums[i]-i]+=1
         return total_pairs-good_pairs
 
         
