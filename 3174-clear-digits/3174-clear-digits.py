5adcacecec36e4c488b9fd462031d6835f921f0f
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-02-10 14:09:59 +0530

    Time: 0 ms (100%), Space: 18 MB (7.88%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        
+        stack=[]
+
+        for i in s:
+            if i >= "0" and i <="9":
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)
