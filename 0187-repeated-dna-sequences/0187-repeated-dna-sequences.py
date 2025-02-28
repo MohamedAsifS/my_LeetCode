4eabefd05fc75323a91684d89f33519ad8ecaff2
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-02-28 18:49:39 +0530

    Time: 21 ms (80.03%), Space: 28.8 MB (76.34%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+      
+        hash={}
+        res=set()
+        l=10
+        print(len(s))
+
+        for i in range(0,len(s)-9):
+            slices=s[i:l]
+            if slices not in hash:
+                hash[slices]=1
+            else:
+                hash[slices]+=1
+                res.add(slices)
+            l+=1
+        return list(res)
+        
