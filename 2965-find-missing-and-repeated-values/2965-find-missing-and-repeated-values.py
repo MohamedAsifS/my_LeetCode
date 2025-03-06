3edad3839c05d1ef67b3f91620a7468adbb093f0
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-03-06 14:40:43 +0530

    Time: 15 ms (31.25%), Space: 18.4 MB (8.94%) - LeetHub

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        hash={}
+
+        for i in range(1,(len(grid[0])**2)+1):
+              hash[i]=False
+        res=[0]*2
+        for i in grid:
+            for j in i:
+                if j in hash and hash[j]==False:
+                    hash[j]=True
+                else:
+                    
+                    res[0]=j
+       
+        for i in hash.keys():
+            print(i)
+            if hash[i]==False:
+
+                res[1]=i
+                break
+        return res
+
+        
+
+        
