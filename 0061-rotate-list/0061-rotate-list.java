534cefdebaaa0bebbc082788c19da83409b259c4
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-03-14 20:08:27 +0530

    Time: 0 ms (100%), Space: 42.6 MB (62.61%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,68 +1,47 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        int len=1;
+        int len = 1;
 
-        if(head==null || head.next==null) return head;
+        if (head == null || head.next == null)
+            return head;
 
-        ListNode templast=head;
+        ListNode templast = head;
 
-        while(templast!=null && templast.next!=null){
+        while (templast != null && templast.next != null) {
             len++;
-            templast=templast.next;
+            templast = templast.next;
         }
 
-       
-       
-        
-        int need=k%len;
-         if (need==0)return head;
-        
-        len=len-need;
-        ListNode temp=head;
+        int need = k % len;
+        if (need == 0)
+            return head;
 
-        
+        len = len - need;
+        ListNode temp = head;
 
-       
-       
-        
-
-        while(len>1){
-            temp=temp.next;
+        while (len > 1) {
+            temp = temp.next;
             len--;
 
         }
-       
+        ListNode join = new ListNode();
+        join.next = temp.next;
+        join = temp.next;
+        temp.next = null;
 
-        ListNode join= new ListNode();
-        join.next=temp.next;
-        join=temp.next;
-        temp.next=null;
-
-        templast.next=head;
-
-        
-
-
-
-
-
-
-
-        
-
-
+        templast.next = head;
 
         return join;
-        
+
     }
 }
