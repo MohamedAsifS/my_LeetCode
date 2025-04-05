d6914213351b9697dfcb5866884aafe89cc1e2b4
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-04-05 17:39:05 +0530

    Runs in 0 ms using 18.7 MB, beating 100% in speed. - LeetRevise

---
+++ 0268-Missing Number/code.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        n=len(nums)
+        t=n*(n+1)//2
+      
+        for i in nums:
+            t-=i
+        return t
+
