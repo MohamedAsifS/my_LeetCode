98058fefe9c79bd9f9231d579cff073ae44abe5e
Mohamed  Asif S <86877461+MohamedAsifS@users.noreply.github.com>
2025-04-11 21:47:56 +0530

    Runs in 565 ms using 17.7 MB, beating 65% in speed. - LeetRevise

---
+++ 2843-  Count Symmetric Integers/code.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        count=0
+        for i in range(low,high+1):
+               s=str(i)
+               if len(s)%2==0:
+                    mid=len(s)//2
+                    if sum(map(int,s[:mid])) == sum(map(int,s[mid:])):
+                        count+=1
+        return count
+        
