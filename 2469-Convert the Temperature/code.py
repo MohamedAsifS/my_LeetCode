fd022f5f6abf1758fe880c1c3bdb006489e6f15f
Asif <86877461+MohamedAsifS@users.noreply.github.com>
2025-05-07 18:57:51 +0530

    Runs in 0 ms using 17.9 MB, beating 100% in speed. - LeetRevise

---
+++ 2469-Convert the Temperature/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+
+        res=[]
+        res.append(celsius+273.15)
+        res.append(celsius*1.80+32)
+        return res
+        
