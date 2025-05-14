df1f2029077af10865fe1852f85ba45dc72944e6
Asif <86877461+MohamedAsifS@users.noreply.github.com>
2025-05-14 21:55:36 +0530

    Runs in 0 ms using 18.1 MB, beating 100% in speed. - LeetRevise

---
+++ 1672-Richest Customer Wealth/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+
+        res=0
+
+        for i in accounts:
+            res=max(res,sum(i))
+        return res
+        
