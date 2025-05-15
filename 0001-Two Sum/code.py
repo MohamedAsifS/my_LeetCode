2618a9c6d4667c80fd7d8b7e0a4ca909a69eba17
Asif <86877461+MohamedAsifS@users.noreply.github.com>
2025-05-16 02:46:37 +0530

    Runs in 0 ms using 19.3 MB, beating 100% in speed. - LeetRevise

--- 0001-Two Sum/code.py
+++ 0001-Two Sum/code.py
@@ -2,9 +2,11 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
       res={}
 
+
       for i in range(len(nums)):
-       
-        if target-nums[i] in res.keys():
-            return [res[target-nums[i]],i]
-        res[nums[i]]=i
+        if nums[i] in res:
+            return [res[nums[i]],i]
+        res[target-nums[i]]=i
+     
+
         
